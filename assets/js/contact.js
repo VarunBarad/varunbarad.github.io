65969622aa2f74f54819031206194c1d310817e6
Varun Barad <varun@varunbarad.com>
2019-11-21 17:32:30 +0530

    Show alert to user when message is either sent successfully or errors out.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -7,14 +7,14 @@ function submitForm(event) {
   var name = document.getElementById('name').value;
   var email = document.getElementById('email').value;
   var message = document.getElementById('message').value;
-  saveMessageToFirebase(name, email, message);
+  sendMessageToServer(name, email, message);
 
   document.getElementById("submit").blur();
   document.getElementById('submit').innerHTML = '<i class=\"fas fa-circle-notch fa-spin\"></i>';
   return false;
 }
 
-function saveMessageToFirebase(name, email, message) {
+function sendMessageToServer(name, email, message) {
   fetch(
       config.messageEndPoint,
       {
@@ -32,9 +32,23 @@ function saveMessageToFirebase(name, email, message) {
     return response.json();
   }).then(function (jsonResponse) {
     console.log(jsonResponse);
+    // Display the message with a timeout of 2.5 seconds to dismiss it
+    var successAlert = '<div><br /><div id="formMessage" class="alert alert-success text-center" role="alert">Your message is received. I will get back to you shortly.</div></div>';
+    document.querySelector('#contactForm').innerHTML += successAlert;
+    window.setTimeout(function () {
+        var contactForm = document.querySelector('#contactForm');
+        contactForm.removeChild(contactForm.lastChild);
+    }, 2000);
     document.getElementById('submit').innerHTML = 'Submit';
   }).catch(function (error) {
     console.error(error);
+    // Display the message with a timeout of 2.5 seconds to dismiss it
+    var errorAlert = '<div><br /><div id="formMessage" class="alert alert-danger text-center" role="alert">Your message could not be sent. Can you please try again after some time?</div></div>';
+    document.querySelector('#contactForm').innerHTML += errorAlert;
+    window.setTimeout(function () {
+        var contactForm = document.querySelector('#contactForm');
+        contactForm.removeChild(contactForm.lastChild);
+    }, 2000);
     document.getElementById('submit').innerHTML = 'Submit';
   });
 }
