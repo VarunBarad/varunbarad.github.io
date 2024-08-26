45bd9af707978c06bcc75f19132b6c406fe7386f
Varun Barad <varun@varunbarad.com>
2024-08-27 01:13:40 +0530

    [WIP] Eleventy: Add blog functionality

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,15 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_eleventy/favicon.png");
   eleventyConfig.addPassthroughCopy("_eleventy/podcasts");
   
+  eleventyConfig.addCollection('categories', function(collectionApi) {
+    let categories = new Set();
+    let posts = collectionApi.getFilteredByTag('post');
+    posts.flatMap(post => post.data.categories).forEach((category) => {
+      categories.add(category);
+    });
+    return Array.from(categories);
+  });
+  
   return {
     dir: {
       layouts: "_layouts",
