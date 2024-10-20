e4c3fc6202504d042d0b874c46ab9ee72d750db3
Varun Barad <varun@varunbarad.com>
2024-10-20 08:46:16 +0530

    Eleventy: Add support for RSS feeds.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,36 @@
+const feedPlugin = require('@11ty/eleventy-plugin-rss');
+const { ZonedDateTime, ZoneId, DateTimeFormatter } = require('@js-joda/core');
+require('@js-joda/timezone');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_eleventy/favicon.png");
   eleventyConfig.addPassthroughCopy("_eleventy/assets");
   eleventyConfig.addPassthroughCopy("_eleventy/podcasts");
+  
+  eleventyConfig.addNunjucksFilter('dateToRfc3339WithTimezone', (inputDateObj) => {
+    return ZonedDateTime.parse(inputDateObj.toISOString())
+      .withZoneSameInstant(ZoneId.of('Asia/Kolkata'))
+      .format(DateTimeFormatter.ofPattern('yyyy-MM-dd^HH:mm:ssXXX'))
+      .replace('^', 'T');
+  });
+  
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: 'atom',
+    outputPath: '/feed.xml',
+    collection: {
+      name: 'post',
+      limit: '0',
+    },
+    metadata: {
+      language: 'en',
+      title: 'Varun Barad - Curious Developer',
+      subtitle: 'My online home where I post about new things I learn or find interesting.',
+      base: 'https://varunbarad.com/',
+      author: {
+        name: 'Varun Barad',
+      },
+    },
+  });
   
   eleventyConfig.addCollection('blogPostCategories', function(collectionApi) {
     const categoryNames = new Set();
